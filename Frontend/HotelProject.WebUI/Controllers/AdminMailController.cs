103bc4406087a2986a5412ac87bddf3d36bd9fea
kenanqedirov <k.qedirov1999@gmail.com>
2023-12-10 00:30:20 +0400

    Added Send Mail to Gmail by Admin

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "k.qedirov1999@gmail.com");     ///   Sender
+            mimeMessage.From.Add(mailboxAddressFrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User",model.ReceiverMail);      ///   Receiver
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;                                                    ///  Message Body
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = model.Subject;                                                  /// Message Subject ( Title )
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("k.qedirov1999@gmail.com", "ktuo aqhm yzod lizc");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();
+        }
+    }
+}
